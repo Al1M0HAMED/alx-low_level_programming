6a1bfb5e24e6bd71115649bc9ac1173bfe03976e
Al1M0hamed <alimohamed525666@gmail.com>
2023-05-18 14:51:13 +0200

    syntx errot and checker compiling request

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,26 +15,24 @@
 
 int main(void)
 {
-	int n, lastdigit;
+	int n, lstdgt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastdigit = n % 10;
+	lstdgt = n % 10;
 
-	if (lastdigit > 5)
+	if (lstdgt > 5)
 	{
-	printf("Last digit of %d is %d and is greater
-			than 5\n", n, lastdigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstdgt);
 	}
-	else if (lastdigit == 0)
+	else if (lstdgt == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lstdgt);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,
-				lastdigit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstdgt);
 	}
 
 	return (0);
