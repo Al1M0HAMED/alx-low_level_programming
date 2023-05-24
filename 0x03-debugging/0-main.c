be348c08121fd8239856d12143608477a0eeca30
Al1M0hamed <alimohamed525666@gmail.com>
2023-05-24 16:43:45 +0200

    positive_or_nigative.c test case

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - this function tests if the function positive_or_negative works.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+}
