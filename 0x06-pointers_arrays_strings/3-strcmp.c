de45ffb3a4bb55f007982d78585da8f69cf5ce09
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-02 01:32:04 +0200

    i think i fixed it ?

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,29 +2,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, s, ss, r;
+	int i, s;
 
-	i = 0, s = 0, ss = 0;
-	while (s1[i] != '\0')
+	i = 0, s = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s == 0)
 	{
-		s += s1[i];
+		s += s1[i] - s2[i];
 		i++;
 	}
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		ss += s2[j];
-		j++;
-	}
-	if (i == j)
-		r = (s - ss);
-	else
-		r = (s - ss);
-	if (r > 0)
-		r = 15;
-	else if (r < 0)
-		r = -15;
-	else 
-		r = 0;
-	return (r);
+	i--;
+	return (s1[i] - s2[i]);	
 }
