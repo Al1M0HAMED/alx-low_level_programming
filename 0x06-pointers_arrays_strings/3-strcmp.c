a1a97d8c953da4f447a68f72de67ae6ef522b2ac
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-02 00:47:52 +0200

    output bug fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,64 +2,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, n, s, ss, r;
+	int i, j, s, ss;
 
-	i = 0, s = 0, ss = 0, j = 0, r = 0;
+	i = 0;
 	while (s1[i] != '\0')
 	{
+		s += s1[i];
 		i++;
 	}
 	j = 0;
 	while (s2[j] != '\0')
-        {
-                j++;
-        }
+	{
+		ss += s2[j];
+		j++;
+	}
 	if (i == j)
-	{
-		n = 0;
-                while (s1[n] != '\0')
-                {
-                        s += s1[n];
-			n++;
-                }
-                n = 0;
-                while (s2[n] != '\0')
-                {
-                	ss += s2[n];
-			n++;
-                }
-		if (s > ss)
-                        r = 15;
-
-                if (ss > s)
-			r = -15;
-
-		if (s == ss)
-			r = 0;
-
-	}
-	else if (i != j)
-	{
-		n = 0;
-		while (s1[n] != '\0')
-		{
-			s += s1[n];
-			n++;
-		}
-		n = 0;
-		while (s2[n] != '\0')
-                {
-                        ss += s2[n];
-			n++;
-                }
-		if (s > ss)
-			r = 15;
-
-		else if (ss > s)
-			r = -15;
-
-		else
-			r = 15;
-	}
-	return (r);
+		return (s - ss);
+	else if (i != j && (s - ss) == 0)
+		return (s);
+	else
+		return (s - ss);
 }
