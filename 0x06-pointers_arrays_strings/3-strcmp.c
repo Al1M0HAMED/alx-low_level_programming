c1eb5337f4b5dbd52d5249cb5d5047f39f1f7e3a
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-01 23:02:57 +0200

    my string compare function initial solve bug fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, j, n, s, ss, equal, r;
 
-	i = 0, s = 0, ss = 0;
+	i = 0, s = 0, ss = 0, equal = 0, r = 0, j = 0;
 	while (s1[i] != '\0')
 	{
 		i++;
@@ -31,7 +31,7 @@ int _strcmp(char *s1, char *s2)
                         r = 15;
                 else if (ss > s)
                         r = -15;
-		else 
+		else if (equal == (n - 1))
 			r = 0;
 
 	}
