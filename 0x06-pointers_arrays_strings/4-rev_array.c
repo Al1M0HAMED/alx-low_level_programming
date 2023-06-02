0cdf3204a7071670b36a347661bb2918eff5e08c
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-02 22:58:57 +0200

    rev_array function implemnetation

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * reverse_array - this function reverse an array elements.
+ *
+ * @a: is pointer to array.
+ *
+ * @n: is integer.
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, l, first_e, last_e;
+
+	if (n % 2 != 0)
+	{
+		l = (n - 1);
+	}
+	l = n / 2;
+	i = 0, j = (n - 1);
+	while (i != l)
+	{
+		last_e = *(a + j);
+		first_e = *(a + i);
+		a[j] = first_e;
+		a[i] = last_e;
+		j--;
+		i++;
+	}
+}
