926239fc3393d117f5b169bbefe0025aa7f9d261
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-03 21:17:37 +0200

    fixing last output issue at captaliazing strings script

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,11 +20,10 @@ char *cap_string(char *str)
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
-				str[i + 1] -= ' ';
+				str[i + 1] -= 32;
 			}
 		}
 		i++;
 	}
-	if (i > 0)
-		return (str);
+	return (str);
 }
