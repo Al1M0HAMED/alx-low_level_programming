23119771bdbee12c23c50f31795082537ad8358b
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-06 16:32:52 +0200

    betty styling

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
+ * _strspn - this function is an implementation of strspn function.
  *
+ * @s: parameter is the source string.
  *
+ * @accept: is the test string.
  *
- *
- *
- *
+ * Return: Always the number of bytes in the initial segment of s.
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	int j, n, Do;
 	unsigned int l, i;
-	i = 0, n = 0, Do = 1;
+
+	i = 0, Do = 1;
 	while (s[i] != '\0' && Do)
 	{
 		j = 0, n = 0;
