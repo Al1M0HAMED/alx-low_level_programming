eea8a2aa7c4c4541cc6f6abe82282f4ae0e2657d
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-06 17:22:26 +0200

    strpbrk function implementation

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strpbrk - this function is an implementation of strpbrk function.
+ *
+ * @s: parameter is array of char.
+ *
+ * @accept: parameter is array of char.
+ *
+ * Return: Always pointer to the byte s.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				break;
+			j++;
+		}
+		if (s[i] == accept[j])
+			break;
+		i++;
+	}
+	return (s + i);
+}
