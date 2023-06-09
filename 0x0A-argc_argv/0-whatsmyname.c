565ea263068c1078b598d4b318e379653cc2f8a9
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-09 13:49:05 +0200

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
+ * main - this program prints its name.
  *
+ * @argc: parameter is int.
  *
+ * @argv: parameter is char pointer to array.
  *
- *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
