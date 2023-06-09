26b2bd2cfe91d28187e725f9ff99083f3bdcc8fa
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-09 03:59:55 +0200

    testy

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,38 +1,34 @@
 #include "main.h"
 int i = 0, n = 1;
 /**
+ * _print_rev_recursion - this fnuction prints a string in reverse in
+ * recursion.
  *
+ * @s: parameter is string.
  *
- *
- *
- *
+ * Return: void.
  */
 void _print_rev_recursion(char *s)
 {
-    if (i == 0 && n == 0)
-    {
-        _putchar(*s);
-	--s;
-	_putchar(*s);
-        return;
-    }
-    if (n)
-    {
-        if (*s == '\0')
-        {
-            n = 0;
-            i--;
-        }
-        else if (*s != '\0')
-        {
-            i++;
-            _print_rev_recursion(++s);
-        }
-    }
-    if (i != -1)
-    {
-        i--;
-        _putchar(*s);
-        _print_rev_recursion(--s);
-    }
+	if (i == -2 && n == 0)
+		return;
+   	if (n)
+	{
+		if (*s == '\0')
+        	{
+			n = 0;
+			i--;
+		}
+        	else if (*s != '\0')
+		{
+			i++;
+			_print_rev_recursion(++s);
+		}
+	}
+	if (i != -3)
+	{
+		i--;
+		_putchar(*s);
+		_print_rev_recursion(--s);
+	}
 }
