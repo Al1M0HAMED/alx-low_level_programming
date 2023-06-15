d1ecd2bcf4c617519751c9d767a2f78b770ef119
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-15 21:28:46 +0200

    initail commit for str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * str_concat - this function concatenates two strings.
+ *
+ * @s1: parameter is pointer to char of array.
+ *
+ * @s2: parameter is pointer to char of array.
+ *
+ * Return: NULL on failure. 
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, size1, size2, size, l;
+	char *p;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	size1 = ((i + 1) * sizeof(char));
+	size2 = ((j + 1) * sizeof(char));
+	size = size1 + size2;
+	if (size <= 0)
+		return (NULL);
+	p = malloc(size);
+	if (p == NULL)
+		return (NULL);
+	j = 0;
+	while (s1[j] != '\0')
+	{
+		p[j] = s1[j];
+		j++;
+	}
+	l = 0;
+	while (s2[l] != '\0')
+	{
+		p[j] = s2[l];
+		l++;
+		j++;
+	}
+	p[j] = '\0';
+	return (p);
+}
