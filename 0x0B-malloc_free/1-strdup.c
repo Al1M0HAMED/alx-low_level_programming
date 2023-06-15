a4a5bde4e6d79d1483588a9cae8f1f37d06f2482
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-15 20:49:15 +0200

    strdup implementation script

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strdup - this is an implementation of function strdup.
+ *
+ * @str: parameter is pointer to array of chars.
+ *
+ * Return: On success, returns a pointer to the duplicated string. It returns
+ *  NULL if insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	unsigned int size, i;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	size = ((i + 1) * sizeof(char));
+	if (size <= 0)
+		return (NULL);
+	p = malloc(size);
+	i = 0;
+	while (i < size)
+	{
+		p[i] = str[i];
+		i++;
+	}
+	return (p);
+}
