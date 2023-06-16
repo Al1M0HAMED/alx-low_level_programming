98263b42cab8094375d7112e47934a6d3a6e72b6
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-16 06:32:29 +0200

    fixing output bug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,34 +13,28 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int s1_l, s2_l, i, j;
+	int nn, i, j;
 
-	j = 0;
-	if (n < j)
+	nn = n;
+	if (nn < 0)
 		return (NULL);
 	if (s1 == NULL)
                 s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (nn > _strlen(s2))
+		nn = _strlen(s2);
 
-	for (s2_l = 0; s2[s2_l] != '\0'; s2_l++)
-		;
-	for (s1_l = 0; s1[s1_l] != '\0'; s1_l++)
-		;
-
-	if (n >= s2_l)
-		n = s2_l;
-
-	p = malloc((n + s1_l + 1) * sizeof(char *));
+	p = malloc((nn + _strlen(s1) + 1) * sizeof(char *));
 
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		p[i] = s1[i];
-	for (j = 0; j < n; i++)
+	for (j = 0; j < nn; j++)
 		p[j + i] = s2[j];
-	p[i] = '\0';
+	p[i + j] = '\0';
 
 	return (p);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+int _strlen(char *s);
 void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
