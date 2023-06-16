177f4b9f5844dd46dc42402723842d0bee725ad0
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-16 05:55:03 +0200

    fixing a output bug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 	unsigned int s1_l, s2_l, i, j;
 
+	if (n <= 0)
+		return (NULL);
 	if (s1 == NULL)
                 s1 = "";
 	if (s2 == NULL)
