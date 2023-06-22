4c2b1ba3cf7bdcfbf3395d8660c76be6a05d63b3
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-22 20:48:32 +0200

    seccond task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - func.
+ * @separator: string.
+ * @n: int
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i, num;
+	va_list list;
+
+	num = n;
+	if (n > 0 && separator != NULL)
+	{
+		va_start(list, n);
+		i = 0;
+		while (i < num)
+		{
+			printf("%d", va_arg(list, int));
+			if (i < (num - 1))
+				printf("%s", separator);
+			i++;
+		}
+		va_end(list);
+		putchar('\n');
+	}
+}
