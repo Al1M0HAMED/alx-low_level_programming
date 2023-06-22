55bc8dbeaaf67dde62c37f4e3789b562cacce50b
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-22 20:50:26 +0200

    seccond task string = NULL bug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,14 +10,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 	num = n;
-	if (n > 0 && separator != NULL)
+	if (n > 0)
 	{
 		va_start(list, n);
 		i = 0;
 		while (i < num)
 		{
 			printf("%d", va_arg(list, int));
-			if (i < (num - 1))
+			if (i < (num - 1) && separator != NULL)
 				printf("%s", separator);
 			i++;
 		}
