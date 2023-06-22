dcb4152b748a11db0e4abdfef45fa65e0b90ae6e
Al1M0hamed <alimohamed525666@gmail.com>
2023-06-23 00:34:16 +0200

    |, | bug fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,42 +5,50 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i, start;
 	char sp[200];
 	va_list list;
 	char *str;
 
 	va_start(list, format);
-	i = 0;
+	i = 0, start = 0;
 	while (format && format[i] != '\0')
 	{
 		sp[i] = format[i];
 		switch (sp[i])
 		{
 			case 'i':
+				switch (start)
+				{ case 1: printf(", "); }
+				start = 1;
 				printf("%d", va_arg(list, int));
 				break;
 			case 's':
+				switch (start)
+				{ case 1: printf(", "); }
+				start = 1;
 				str = va_arg(list, char *);
 				if (str != NULL)
 				{
+
 					printf("%s", str);
 					break;
 				}
 				printf("(nil)");
 				break;
 			case 'f':
+				switch (start)
+				{ case 1: printf(", "); }
+				start = 1;
 				printf("%f", va_arg(list, double));
 				break;
 			case 'c':
+				switch (start)
+				{ case 1: printf(", "); }
+				start = 1;
 				printf("%c", va_arg(list, int));
 				break;
-			default:
-				break;
 		}
-		if ((sp[i] == 'i' || sp[i] == 's' || sp[i] == 'f' || sp[i] == 'c')
-					&& format[i + 1] != '\0')
-				printf(", ");
 		i++;
 	}
 	va_end(list);
