3482c35adba3f1f10cc28b589cef0c0a55548411
Ali Mohamed <mohamed37222721@gmail.com>
2023-07-01 01:33:10 +0200

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_node - add node to a list.
+ * @head: a pointer to the head of the list.
+ * @str: is a pointer to const char.
+ * Return: pointer t new node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *s;
+	int length = 0;
+
+	s = malloc(sizeof(list_t));
+	if (s == NULL)
+		return (NULL);
+	for (; str[length] != '\0'; length++)
+	s->str = strdup(str);
+	s->len = length;
+	s->next = *head;
+	*head = s;
+	return ((*head)->next);
+}
