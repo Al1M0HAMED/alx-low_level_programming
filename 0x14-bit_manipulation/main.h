6fc44a6dfbda9b2120a8ab940a750c53ec7307bf
Al1M0HAMED <alimohamed525666@gmail.com>
2023-08-06 08:02:01 +0300

    get rid of unused functions

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,12 +4,7 @@
 #include <limits.h>
 #include <stdio.h>
 
-unsigned int decimal_to_binary(char *binary, unsigned long int decimal);
-unsigned int decimal_too_binary(char *binary, unsigned long int *decimal);
-void rev_string(char *s);
-char *conv_dic(int decimal, char *binary);
 unsigned int binary_to_uint(const char *b);
-unsigned long int *binary_too_uint(char *str);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
