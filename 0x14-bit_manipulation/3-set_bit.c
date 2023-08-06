b4a3621259aa5f80f353c30cc417282cb7191083
Al1M0HAMED <alimohamed525666@gmail.com>
2023-08-06 04:33:40 +0300

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,42 @@
+#include "main.h"
+int _pow_recursion(int x, unsigned int y);
+/**
+ * set_bit - is siting bit to 1
+ * @n: is the unit.
+ * @index: is the index.
+ * Return: 1 or -1.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int i = 0, bit;
+
+	if (index > 64)
+		return (-1);
+	bit = (*n >> index);
+	if ((bit & 1) == 0)
+		i = _pow_recursion(2, index);
+	*n = *n + i;
+	return (1);
+}
+/**
+ * _pow_recursion - this function calc the val of x raised to the power of y.
+ *
+ * @x: parameter is int.
+ *
+ * @y: parameter is int.
+ *
+ * Return: If y is lower than 0, the function returns -1 else returns the val
+ * of x raised to the power of y.
+ */
+int _pow_recursion(int x, unsigned int y)
+{
+	if (y <= 0)
+	{
+		if (y == 0)
+			return (1);
+		else
+			return (-1);
+	}
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
