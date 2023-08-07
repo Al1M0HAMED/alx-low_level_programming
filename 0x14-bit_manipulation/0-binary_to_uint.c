47a52e9050c7c1f8704edaef7852e5107e20e914
Al1M0HAMED <alimohamed525666@gmail.com>
2023-08-07 07:00:10 +0300

    extreme readability & testabilty & maintianablity

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,24 +6,22 @@
  */
 unsigned int binary_to_uint(const char *binary)
 {
-	const char *b = binary;
-	int power = 0;
-	unsigned int bit, unit = 0;
+	const char *binary_start = binary;
+	int current_bit_power = 0;
+	unsigned int current_bit_value, binary_value, result = 0;
 
-	if (binary != NULL)
+	while (*binary != '\0' && binary != NULL)
+		binary++;
+	binary--;
+	while (binary >= binary_start && binary != NULL)
 	{
-		while (*binary != '\0')
-			binary++;
+		if (*binary < '0' || *binary > '1')
+			return (0);
+		binary_value = *binary - '0';
+		current_bit_value = binary_value * (1 << current_bit_power);
+		result = result + current_bit_value;
+		current_bit_power++;
 		binary--;
-
-		while (binary >= b)
-		{
-			if (*binary < '0' || *binary > '1')
-				return (0);
-			bit = (*binary - '0') * (1 << power);
-			unit = unit + bit;
-			power++, binary--;
-		}
 	}
-	return (unit);
+	return (result);
 }
