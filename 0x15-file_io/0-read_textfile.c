5be7c2b26a9cf7113e9b87ce676bc007bfa99d0b
Al1M0HAMED <alimohamed525666@gmail.com>
2023-08-11 09:12:17 +0300

    tests 0.2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,24 +11,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t number_of_bytes_read, number_of_bytes_written;
 	char *buff;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_RDONLY);
 	buff = malloc(sizeof(char) * letters + 1);
 	number_of_bytes_read = read(fd, buff, letters);
 	if (number_of_bytes_read == -1 || buff == NULL || filename == NULL
 			|| letters == 0 || fd < 0)
 	{
-		close(fd);
 		free(buff);
 		return (0);
 	}
-	number_of_bytes_written = write(1, buff, number_of_bytes_read);
+	buff[number_of_bytes_read] = '\0';
+	close(fd);	
+	number_of_bytes_written = write(STDOUT_FILENO, buff, 
+			number_of_bytes_read);
 	if (number_of_bytes_written != number_of_bytes_read)
 	{
-		close(fd);
 		free(buff);
 		return (0);
 	}
-	close(fd);
 	free(buff);
 	return (number_of_bytes_read);
 }
