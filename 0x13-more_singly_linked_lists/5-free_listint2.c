0f503b4e908f5650760128f3760e2574f4e45a26
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-09 02:09:05 +0300

    m

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,17 +2,18 @@
 /**
  * free_listint2 - Frees a list.
  * @head: Address of the first node of a list.
- **/
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
 	temp = *head;
+	if (!*head)
+		return;
 	while (temp != NULL)
 	{
 		*head = (*head)->next;
 		free(temp);
 		temp = *head;
 	}
-	*head = NULL;
 }
