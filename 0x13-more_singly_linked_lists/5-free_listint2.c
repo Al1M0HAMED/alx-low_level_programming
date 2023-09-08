c8c98d296e21b6fa57b9ac80c29242e7c08d1e94
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-09 02:00:01 +0300

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,8 @@
 #include "lists.h"
-#include <stdlib.h>
-
 /**
- * free_listint - Frees a list.
+ * free_listint2 - Frees a list.
  * @head: Address of the first node of a list.
  **/
-
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
@@ -17,4 +14,5 @@ void free_listint2(listint_t **head)
 		free(temp);
 		temp = *head;
 	}
+	*head = NULL;
 }
