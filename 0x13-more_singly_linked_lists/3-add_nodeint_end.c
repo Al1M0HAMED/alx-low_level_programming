4f8e4d8bee0181dabd503eb59a5d3d2c8b048e41
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-09 00:49:03 +0300

    hot fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,40 +5,24 @@
  * @head: is the linked list address.
  * Return: the new node address.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **h, const int n)
 {
-	size_t len;
-	listint_t *new_node;
+	listint_t *new_node, *head;
 
+	head = *h;
 	new_node = malloc(sizeof(new_node));
-	new_node->n = n;
 	if (!new_node)
 		return (NULL);
-	len = listint_len(*head);
-	if (len == 0)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-	len -= 1;
-	while(len >= 1)
-	{
-		*head = (*head)->next;
-		len--;
-	}
-	(*head)->next = new_node;
-	return (new_node);
-}
-/**
- * listint_len - this function returns the number of elements in a linked.
- * @h: is the address of a linked list.
- * Return: the len of a linked list.
- */
-size_t listint_len(const listint_t *h)
-{
-	size_t len = 0;
 
-	while (h != NULL)
-		len++, h = h->next;
-	return (len);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!head)
+		head = new_node;
+	else
+	{
+		while(head->next)
+			head = head->next;
+		head->next = new_node;
+	}
+	return (new_node);
 }
