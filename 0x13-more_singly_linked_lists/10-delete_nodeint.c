e1befe111bd1307b99cb11586c87610e76c9b48e
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-10 16:11:23 +0300

    testing _-_

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	i = 0;
-	while (i < index && current != NULL)
+	while (i < index - 1 && current != NULL)
 	{
 		current = current->next;
 		i++;
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	temp = current->next;
-	free(current);
-	current = temp;
+	current->next = temp->next;
+	free(temp);
 	return (1);
 }
