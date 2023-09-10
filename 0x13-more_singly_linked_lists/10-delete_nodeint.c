092c414fb75ea7155adc46dd0683e3c80d754b9e
Al1M0HAMED <alimohamed525666@gmail.com>
2023-09-10 16:01:28 +0300

    testing

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - this function deletes node at index.
+ * @head: is the address of the linked list.
+ * Return: 1 on success and 0 if failed.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *current = *head, *temp = NULL;
+
+	if (!*head)
+		return (0);
+
+	i = 0;
+	while (i < index && current != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+		return (0);
+
+	temp = current->next;
+	free(current);
+	current = temp;
+	return (1);
+}
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,22 @@
 #include "lists.h"
+
 /**
- * free_listint2 - Frees a list.
- * @head: Address of the first node of a list.
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
-	if (*head != NULL)
+	if (head == NULL)
 		return;
+
 	while (*head)
 	{
-		*head = (*head)->next;
-		free(temp);
-		temp = *head;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+
+	*head = NULL;
 }
