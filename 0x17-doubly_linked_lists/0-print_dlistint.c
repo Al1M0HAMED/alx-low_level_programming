733655e0ecbe77e4634c8b11bdd72c5aa06562ba
AL1M0HAMED <mohamed37222721@gmail.com>
2024-04-07 07:16:07 +0200

    fiend

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,21 @@
 #include "lists.h"
 /**
- * print_dlistint - this function prints data out of linked list.
- * @h: the list.
- * Return: the length of the list.
+ *
+ *
+ *
+ *
+ *
+ *
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t length = 0;
+	size_t number_of_nodes = 0;
 
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
-		h = h->next, length++;
+		printf("%i\n",h->n);
+		h = h->next;
+		number_of_nodes++;
 	}
-	return (length);
+	return (number_of_nodes);
 }
