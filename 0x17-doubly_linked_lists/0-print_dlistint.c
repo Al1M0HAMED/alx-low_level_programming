f1808536b4561efc634aaaf6727898406679b953
AL1M0HAMED <mohamed37222721@gmail.com>
2024-04-07 07:47:00 +0200

    give it to me

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,8 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
- *
- *
+ * print_dlistint - print list element's and nubers.
+ * @h: plist.
+ * Return: size_t value.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
@@ -13,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%i\n",h->n);
+		printf("%i\n", h->n);
 		h = h->next;
 		number_of_nodes++;
 	}
