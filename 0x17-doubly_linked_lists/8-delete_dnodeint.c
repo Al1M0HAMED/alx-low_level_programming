24aabb67962d2d80c145be745b214735749b890d
Al1m0hamed <mohamed37222721@gmail.com>
2024-05-23 08:32:50 +0300

    betty and last finsh

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index -  inserts a new node at a given position.
+ * delete_dnodeint_at_index -  delets node at a given position.
  * @h: is the head of the list.
  * @idx: is the index the new node should be at this idx.
- * @n: is the the data of the new node.
  *
  * Return: adrees of the new_node or NULL if failed.
  */
@@ -23,10 +22,10 @@ int delete_dnodeint_at_index(dlistint_t **h, unsigned int idx)
 			temp->prev = current_node->prev;
 		}
 		if (current_node->prev)
-                {
-                        temp = current_node->prev;
-                        temp->next = current_node->next;
-                }
+		{
+			temp = current_node->prev;
+			temp->next = current_node->next;
+		}
 		else
 			*h = current_node->next;
 
