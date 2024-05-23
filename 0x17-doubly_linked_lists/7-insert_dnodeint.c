1b71520e14a0c82a1a702f6f9923da4c1ab5c418
Al1m0hamed <mohamed37222721@gmail.com>
2024-05-23 04:10:16 +0300

    empty list bug

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	if (!idx)
 	{
-		(*h)->prev = new_node;
+		if (*h)
+			(*h)->prev = new_node;
 		new_node->next = *h;
 		new_node->prev = NULL;
 		*h = new_node;
