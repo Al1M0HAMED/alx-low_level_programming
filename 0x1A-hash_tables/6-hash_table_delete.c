ecdc3da84c607a6251af27253cb198ea8cd16498
Ali Mohamed <alimohamed@192.168.1.16>
2026-04-13 01:23:30 +0200

    leaks for the win

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes hash table.
+ * @ht: is the hash table.
+ * Return: void.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	hash_node_t *tmp1;
+	unsigned long int i;
+
+	i = 0;
+	while (i < ht->size)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			tmp1 = tmp;
+			tmp = tmp->next;
+			free(tmp1->key);
+			free(tmp1->value);
+			free(tmp1);
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
